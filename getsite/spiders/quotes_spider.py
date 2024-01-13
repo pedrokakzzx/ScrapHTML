76c170933bc4068d32c34910530633448a5516a1
pedrokakzzx <138934489+pedrokakzzx@users.noreply.github.com>
2024-01-13 14:36:04 -0300

    Update quotes_spider.py

--- getsite/spiders/quotes_spider.py
+++ getsite/spiders/quotes_spider.py
@@ -5,25 +5,25 @@ import colorama
 import scrapy
 import os
 
-
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
         os.system("cls")
         os.system("clear")        
-        print("Enter a website link bellow:")
-        
+        print("Enter a website link below:")
+
         link = input("Link:")
-        urls = [
-            link
-        ]
+        urls = [link]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        save_folder = Path("getsite\saved_pages")
+        current_directory = os.getcwd()
+        save_folder = Path(current_directory) / "getsite" / "saved_pages"
         save_folder.mkdir(parents=True, exist_ok=True)
+        
         filename = save_folder / f"page-{response.url.split('/')[-1]}.html"
         filename.write_bytes(response.body)
+        
         self.log(f"Saved file {filename}")
